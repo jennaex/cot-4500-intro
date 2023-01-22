755a354a501c53a5731035501f9e4c1611e54d35
Jenna Exantus <exanj@knights.ucf.edu>
2023-01-22 13:04:36 -0500

    Update to files

--- source/main/intro_to_python.py
+++ source/main/intro_to_python.py
@@ -1,1 +1,46 @@
-print("Hello, World!")
+#importing numpy
+import numpy as np
+
+if __name__ == '__main__':
+    #creating an array using np.ogrid
+    i, j = np.ogrid[:3, :3]
+
+    #For question 1, use np.where to create a conditional statement in the format (condition, if so then, else)
+    Q1 = np.where(i == j, 1 , 0)
+    print (Q1, "\n")
+
+    #For quesion 2, use the np.where condition to add 3 to each cell
+    Q2 = np.where(i != j, Q1 + 3 , Q1 + 0)
+    print (Q2, "\n")
+
+    #For question 3, use np.delete to delte the last column of Q2
+    Q3 = np.delete(Q2,-1,1)
+    print (Q3, "\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
